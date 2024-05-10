8dfaf3b4dd266762f11e953a90bc6a0102ab9b20
Angad Batra <114503992+angadsbatra1@users.noreply.github.com>
2024-05-10 13:59:02 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -1,9 +1,18 @@
-given a Database and multiple Tables
+-- Given a Database and Multiple Tables
 
-retreiving data:
+-- retrieving data
+SELECT [coloumn, column...] FROM [table];
 
-SELECT [coloumn, column...] FROM [table]
+-- to retreive all from a table:
 
-to retreive all from a table:
+SELECT * FROM [table];
 
-SELECT * FROM [table]
+-- to retrieve all unique values from a table;
+
+SELECT DISTINCT [column] FROM [table];
+
+-- COUNT: counts the number of rows (representing an entity in a table with multiple columns (characteristics)
+-- matching a condiation (which can be left blank if needed)
+
+
+
