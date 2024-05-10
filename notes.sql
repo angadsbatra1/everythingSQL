1509d433febe030a7d4454acb8c86e843567f407
Angad Batra <114503992+angadsbatra1@users.noreply.github.com>
2024-05-10 14:24:38 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -1,3 +1,5 @@
+-- DO NOT RUN
+
 -- Given a Database and Multiple Tables
 
 -- retrieving data
@@ -13,6 +15,11 @@ SELECT DISTINCT [column] FROM [table];
 
 -- COUNT: counts the number of rows (representing an entity in a table with multiple columns (characteristics)
 -- matching a condiation (which can be left blank if needed)
+
+
+SELECT COUNT(*) FROM [table] -- returns the number of rows (representing entities)
+
+SELECT COUNT(column) FROM [table] -- returns the number of rows in a coloumn (excluding rows/entities with a NULL value for that column/characteristic)
 
 
 
