b7fe052c475479a87ce4cf627faeeddc7f2d8ed7
Angad Batra <114503992+angadsbatra1@users.noreply.github.com>
2024-05-15 21:55:30 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -3,7 +3,7 @@
 -- Given a Database and Multiple Tables
 
 -- retrieving data
-SELECT [coloumn, column...] FROM [table];
+SELECT [column, column ...] FROM [table];
 
 -- to retreive all from a table:
 
@@ -23,10 +23,28 @@ SELECT COUNT(column) FROM [table] -- returns the number of rows in a coloumn (ex
 
 SELECT COUNT DISTINCT [column] FROM [table]
 
+
 ORDER BY -- useful for sorting data, can sort one or more coluumns
 
-SELECT * FROM [table] ORDER BY [coloumn, coloumn] 
+SELECT * FROM [table] ORDER BY [column, column] 
 
-SELECT * FROM [table] ORDER BY [coloumn, coloumn] ASC; -- ascending
+SELECT * FROM [table] ORDER BY [column, column] ASC; -- ascending
 
-SELECT * FROM [table] ORDER BY [coloumn, coloumn] DESC; -- descending
+SELECT * FROM [table] ORDER BY [column, column] DESC; -- descending
+
+
+
+LIMIT -- limits the number of rows that are returned
+
+SELECT * FROM [table] ORDER BY [column] ASC LIMIT [number_of_rows_you_need]
+
+
+WHERE, NOT, BETWEEN -- filter data within a specified range
+
+SELECT * FROM [table] WHERE [coloumn] BETWEEN # AND # LIMIT #;
+
+SELECT * FROM [table] WHERE [coloumn] NOT BETWEEN # AND # LIMIT #;
+
+SELECT * FROM [table] WHERE [coloumn]  BETWEEN 'CONDITION' AND 'CONDITION' LIMIT #;
+
+
