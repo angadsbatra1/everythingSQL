822108066fddd9420191b9476197a1ac41ccf45a
Angad Batra <114503992+angadsbatra1@users.noreply.github.com>
2024-05-15 21:42:23 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -14,12 +14,19 @@ SELECT * FROM [table];
 SELECT DISTINCT [column] FROM [table];
 
 -- COUNT: counts the number of rows (representing an entity in a table with multiple columns (characteristics)
--- matching a condiation (which can be left blank if needed)
+-- matching a condition (which can be left blank if needed)
 
 
 SELECT COUNT(*) FROM [table] -- returns the number of rows (representing entities)
 
 SELECT COUNT(column) FROM [table] -- returns the number of rows in a coloumn (excluding rows/entities with a NULL value for that column/characteristic)
 
+SELECT COUNT DISTINCT [column] FROM [table]
 
+ORDER BY -- useful for sorting data, can sort one or more coluumns
 
+SELECT * FROM [table] ORDER BY [coloumn, coloumn] 
+
+SELECT * FROM [table] ORDER BY [coloumn, coloumn] ASC; -- ascending
+
+SELECT * FROM [table] ORDER BY [coloumn, coloumn] DESC; -- descending
